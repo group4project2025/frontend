0675b0a0442cdd87a19e9f2da1090bfcdd6fb4d4
group4project2025 <group4.project2025@gmail.com>
2026-05-03 17:44:48 +0530

    Update Eligibility.jsx

--- src/pages/Eligibility.jsx
+++ src/pages/Eligibility.jsx
@@ -42,7 +42,7 @@ export default function Eligibility() {
 
     try {
       // NOTE: Backend endpoint /api/send-emails needs to be implemented on port 8000
-      const res = await fetch("http://127.0.0.1:8000/api/send-emails", {
+      const res = await fetch("https://tpc-connect.onrender.com/api/send-emails", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
